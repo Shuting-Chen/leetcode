fde24bb34dea14a3647716dee52ca862c7d5731c
MaggieT <schen150@binghamton.edu>
2016-08-24 20:10:05 -0400

    Create Remove Duplicates from Sorted Array.cpp

---
+++ Remove-Duplicates-from-Sorted-Array/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()<=1) return nums.size();
+        int idx=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]!=nums[i-1])  
+            {
+                nums[++idx]=nums[i];
+            }
+        }
+        return idx+1;
+    }
+};
